f05adc5dc8497000f611f19b3015507b1caba541
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-01-10 00:04:32 +0900

    20230110

---
+++ app.js
@@ -0,0 +1,46 @@
+// 関数・・・繰り返し使われる一連の処理を1つにまとめたもの
+
+//関数書き方
+// function 関数名(){
+//   処理内容
+// }
+
+
+//引数と戻り値
+// 引数・・・入力する値
+// 戻り値・・出力される値
+
+//書き方
+// function 関数名(引数){
+//   処理
+//   return 戻り値;
+// }
+//→これで関数の処理結果が呼び出し元から利用できるようになる
+
+
+//簡単な関数を記述する
+//function addString(strA){
+//  //functionの後に関数名「addString」を記述し引数として「str」を定義している
+//  let addStr = "Hello " + strA;
+//  //変数名「addStr」に「Hello」の文字列と関数の引数「strA」に設定されている文字列を結合している
+//  return addStr;
+//  //結合結果の格納された「addStr」を関数の戻り値としている。returnを記述することで出力結果が表示される
+//}
+//→この状態では関数を作成しただけ
+
+
+//作成した関数を呼び出す
+// メイン部分
+//  let alertString;
+// 作成した関数を呼び出し、変数へ格納
+//  alertString = addString("WebCamp");
+//
+// 変数の中身をアラートで表示する
+//  (alertString);
+//
+// 作成した関数
+//  function addString(strA){
+//    let addStr = "Hello " + strA;
+//    return addStr;
+//  }
+//アラートウィンドウに「Hello WebCamp」の文字が表示される
