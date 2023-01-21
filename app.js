378ae2d2ac64ba4ddfa868c71e82983c685e3203
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-01-21 23:25:00 +0900

    20230121

--- app.js
+++ app.js
@@ -63,7 +63,8 @@
 // function getJShand(){
 //   let js_hand_num = Math.floor( Math.random() * 3 );
 //   乱数を生成する関数を設定。これによりグー・チョキ・パーがランダムに表示されるようにする
-//   Math.floor( Math.random() * 3 )・・・0～2までのランダムな整数値を生成する
+//   Math.floor( Math.random() * 3 )・・・0～2までのランダムな整数値を生成する　※0スタート
+//   関数内の「3」はランダム生成したい整数値の＋1を指定する
 //
 //   if(js_hand_num == 0){
 //     js_hand = "グー";
@@ -74,7 +75,72 @@
 //   }
 //
 //   return js_hand;
+//   ランダム生成された数値の戻り値としてグー・チョキ・パーを出しわける
 // }
+
+
+//↓入力されたグー・チョキ・パーと、JavaScriptで生成したランダムなじゃんけんの手を比べ、勝ち・負け・引き分けの結果を表示
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+//let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//
+// じゃんけんの手をランダムに作成する関数を呼び出す
+//let js_hand = getJShand();
+//
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+//let judge = winLose(user_hand, js_hand);
+//
+// 結果を表示する
+//alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+//
+// ランダムでじゃんけんの手を作成する関数
+//function getJShand(){
+//  let js_hand_num = Math.floor( Math.random() * 3 );
+//  let hand_name;
+//
+//  if(js_hand_num == 0){
+//    hand_name = "グー";
+//  } else if(js_hand_num == 1){
+//    hand_name = "チョキ";
+//  } else if(js_hand_num == 2){
+//    hand_name = "パー";
+//  }
+//
+//  return hand_name;
+//}
+//
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+//function winLose(user, js){
+//  let winLoseStr;
+//
+//  if(user == "グー"){
+//    if(js == "グー"){
+//      winLoseStr = "あいこ";
+//    } else if(js == "チョキ"){
+//      winLoseStr = "勝ち";
+//    } else if(js == "パー"){
+//      winLoseStr = "負け";
+//    }
+//  } else if(user == "チョキ"){
+//    if(js == "グー"){
+//      winLoseStr = "負け";
+//    } else if(js == "チョキ"){
+//      winLoseStr = "あいこ";
+//    } else if(js == "パー"){
+//      winLoseStr = "勝ち";
+//    }
+//  } else if(user == "パー"){
+//    if(js == "グー"){
+//      winLoseStr = "勝ち";
+//    } else if(js == "チョキ"){
+//      winLoseStr = "負け";
+//    } else if(js == "パー"){
+//      winLoseStr = "あいこ";
+//    }
+//  }
+//
+//  return winLoseStr;
+//}
+
 
 
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
