68c10ced72a010343660b04d002260f25e8d000f
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-01-22 23:33:06 +0900

    20230122-1

--- app4.js
+++ app4.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
   $('body').html('<h1>Hello jQuery!!</h1>');
 });
+
+
+
+//jQueryを使うときのポイント
+// ①jQueryサイトからjquery.min.js(最新のもの)をダウンロードする
+//$(document).ready(function () {
+//  // jQueryプログラムの内容
+//});
