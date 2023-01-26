39ecf8e65ed5eaeaa4af28201f086c1a6dd16867
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-01-26 23:54:15 +0900

    20230126

--- app4.js
+++ app4.js
@@ -29,10 +29,28 @@
 //パラメータ・・・メソッドにパラメータを指定すると、その内容に書き換えられる('<h1>Hello jQuery!!</h1>')
 
 
+//【CSSメソッドの書き方】
+//$('セレクタ').css({
+//  'プロパティ1': '値1',
+//  'プロパティ2': '値2',
+//
+//  :
+//  :
+//
+//  'プロパティn': '値n'
+//});
 
-(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
-  });
+
+//$(function(){
+//  $('.box1').css({
+//    'background-color': '#0000FF',
+//    'height': '100px'
+//  //$('セレクタ').css('background-color','#0000FF');
+//  });
+//});
+
+//↓
+
+$(function(){
+  $('.box1').slideDown();
 });
