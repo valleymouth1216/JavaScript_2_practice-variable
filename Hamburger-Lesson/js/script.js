06e9fa09f3cb59f5852a892a3a88831976f132a0
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-02-09 23:57:44 +0900

    20230209

--- Hamburger-Lesson/js/script.js
+++ Hamburger-Lesson/js/script.js
@@ -1,7 +1,11 @@
 $(function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
+    //.toggleClass()は、指定されているclass（ここではactive）をトグル処理するメソッド
+    //トグル処理では、該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加さる
     $('#sp-menu').fadeToggle();
+    //.fadeToggle()は、要素のフェードイン・フェードアウトを切り替えるメソッド
+    //このメソッドを使って、ハンバーガーメニューを開いたときの状態を設定
     event.preventDefault();
   });
 });
