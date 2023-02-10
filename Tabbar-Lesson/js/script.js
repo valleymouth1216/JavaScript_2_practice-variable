c29fdb1944697af0cd1774c61b0531dd6ee48f6b
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-02-10 23:57:47 +0900

    20230210

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,11 +1,10 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    //.toggleClass()は、指定されているclass（ここではactive）をトグル処理するメソッド
-    //トグル処理では、該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加さる
-    $('#sp-menu').fadeToggle();
-    //.fadeToggle()は、要素のフェードイン・フェードアウトを切り替えるメソッド
-    //このメソッドを使って、ハンバーガーメニューを開いたときの状態を設定
-    event.preventDefault();
-  });
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
+
