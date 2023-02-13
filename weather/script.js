1f7181e7847295dac2075a446acba44fdced27d6
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2023-02-13 23:53:02 +0900

    20230213

---
+++ weather/script.js
@@ -0,0 +1,10 @@
+// （API_KEY には、"取得したAPIキー" を記述）
+const API_KEY = "取得したAPIキー";
+$(function(){
+  $('#btn').on('click', function() {
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    })  });
+});
